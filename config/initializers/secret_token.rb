2bb30ed86c0bc31f6f08df3870379654cb456545
Hussein Morsy <hussein@morsy.de>
2011-09-05 06:49:17 +0200

    rails new sohbet -d postgresql -T --old-style-hash

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sohbet::Application.config.secret_token = 'e98d489764eb530d0add52076b69717d1d9707eb11ac09321fa2f7a481528fb565eb9e7c6acfab7832710aa921db3068f20cdcbaa363a529abb2af50fbd83ef7'
