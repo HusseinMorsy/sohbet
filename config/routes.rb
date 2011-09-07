a7b998f53a82d2211ec8046ac4ff91a1fcd66740
Hussein Morsy <hussein@morsy.de>
2011-09-07 22:36:00 +0200

    Added dashboard feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sohbet::Application.routes.draw do
+  get "dashboard/index"
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Sohbet::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ features/dashboard.feature
@@ -0,0 +1,10 @@
+Feature: Dashboard
+  In order to see an overview of the project
+  A User
+  Should be able to the the last messages and meetings
+
+  Scenario: User is not signed in
+    Given I am not logged in
+    When I go to the home page
+    Then I should see "Dashboard"
+    And I should see "Welcome to the Sohbet app"
