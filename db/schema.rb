7253e8456dc44bfcf5b4683342d49fc314f93d0e
Hussein Morsy <hussein@morsy.de>
2011-09-20 11:03:31 +0200

    Updated schema for missing meetings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110918135732) do
+ActiveRecord::Schema.define(:version => 20110920020925) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
     t.integer  "message_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "meetings", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "date"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
